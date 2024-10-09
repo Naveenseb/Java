ba6fd8cbbc4b6db3c7dc7e18403aa1730261c14e
Naveenseb <naveenspananthanam0705@gmail.com>
2024-10-09 15:06:08 +0530

    js

---
+++ Front-end Java/String.js
@@ -0,0 +1,13 @@
+
+let str1="Hello, World";
+
+let str2="this is demo";
+
+let str3='123';
+
+console.log(typeof str1);
+
+console.log(`1+2=${1+2}`);
+
+console.log(str1.length);
+console.log(str1[0]);
